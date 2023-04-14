46fe4e735ed1e1d3c7471b7d38ef0c01839397fd
Tanuj Sengupta <senguptatanuj@gmail.com>
2023-04-14 18:23:43 +0530

    Added Side Drawer

--- src/App.js
+++ src/App.js
@@ -38,24 +38,18 @@ import Slide from '@mui/material/Slide';
 import Library from './components/Library/Library';
 import logo from './logo.svg';
 import './App.css';
+import SideNav from './components/SideNav/SideNav';
 
 const label = { inputProps: { 'aria-label': 'Color switch demo' } };
 
 export default function App() {
+
 	return (
 		<div className="App">
 			<Box sx={{ flexGrow: 1 }}>
 				<AppBar position="fixed">
 					<Toolbar>
-						<IconButton
-							size="large"
-							edge="start"
-							color="inherit"
-							aria-label="menu"
-							sx={{ mr: 2 }}
-						>
-							<MenuIcon />
-						</IconButton>
+						<SideNav/>
 						<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
 							CodeBooker
 						</Typography>
