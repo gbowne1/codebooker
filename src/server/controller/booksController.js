8b792c07dda70c3371f4a29d5fd72b417a8db184
LOGESH-B <logeshbommannan@gmail.com>
2023-07-22 11:14:31 +0530

    backend/bookmodel/init

---
+++ src/server/controller/booksController.js
@@ -0,0 +1,25 @@
+
+const Book = require('../model/booksModel');
+
+module.exports.getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.status(200).json(books);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+  }
+}
+
+module.exports.newBook = async (req, res) => {
+  const { title, author, category, publisher, ISBN, description, edition} = req.body;
+
+  try {
+    const book = new Book({ title, author,  publisher, ISBN, description, edition});
+    await book.save();
+    res.status(200).json(book);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server Error' });
+  }
+}
---
+++ src/server/model/booksModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  description: { type: String },
+  publisher: { type: String },
+  rating: { type: Number },
+  ISBN: { type: Number, unique:  true },
+  edition: { type: Number },
+  reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
+  createdAt:{type:Date,default:Date.now}
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
