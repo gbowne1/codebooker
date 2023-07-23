1619d9f2dc6c2f652001e50614bb427639eeb0ff
Greg Bowne <gbowne144@gmail.com>
2023-07-23 13:54:25 -0700

    Fixing the ESLint and Prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,39 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'standard',
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-  ],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
+    env: {
+        browser: true,
+        es2021: true,
     },
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', 'prettier'],
-  rules: {
-    'jsx-quotes': ['error', 'prefer-single'],
-    quotes: ['error', 'single'],
-    'prettier/prettier': ['error', { singleQuote: true }],
-  },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    extends: [
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+        'react-app',
+        'react-app/jest',
+    ],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+        'jsx-quotes': ['error', 'prefer-single'],
+        quotes: ['error', 'single'],
+        'prettier/prettier': ['error', { singleQuote: true }],
+        'react/react-in-jsx-scope': 'off',
+    },
 };
