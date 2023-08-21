cff1d06a825635487c5d32462fd29fe390d4c17f
BlackBond06 <eleazuosita007@yahoo.com>
2023-08-21 14:36:48 +0100

    Implement forgot password functionality and send reset link via email

--- src/server/controller/userController.js
+++ src/server/controller/userController.js
@@ -1,8 +1,10 @@
 const User = require('../model/userModel');
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
+const nodemailer = require('nodemailer');
+
 const LocalStrategy = require('passport-local').Strategy;
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 //session passport
 passport.use(
@@ -107,4 +109,108 @@ module.exports.logout = async (req, res) => {
         }
         res.status(200).json('Successfully Logged Out');
     });
+};
+module.exports.forgotPassword = async (req, res) => {
+    const { email } = { ...req.body };
+    try {
+        const existinguser = await User.findOne({ email });
+
+        if (!existinguser) {
+            return res.status(404).json({
+                message:
+                    'Failed to load resource: the server responded with a status of 404 (Not Found)',
+            });
+        }
+
+        // Checks if resetPasswordToken exist in existinguser collection, delete it.
+        if (existinguser.resetPasswordToken) {
+            await User.updateOne(
+                { _id: existinguser._id },
+                { $unset: { resetPasswordToken: 1 } }
+            );
+        }
+        // Create new token
+        const resetToken = jwt.sign(
+            { id: existinguser._id },
+            'codebooker',
+            { expiresIn: '30m' } //30min
+        );
+
+    
+        //Update existing user collection with new reset token
+        await User.updateOne(
+            { _id: existinguser._id },
+            { $set: { resetPasswordToken: resetToken } }
+        );
+
+        // URL link
+        const resetLink = `http://localhost:3000/reset-password/${resetToken}`;
+        
+        // message to send user via nodemailer
+        const message = `
+            <h2>Hello ${existinguser.username}</h2>
+            <p>Please use the url below to reset your password</p>
+
+            <a href=http://localhost:3000/reset-password/${resetToken}>${resetLink}</a>
+            <p>Regards</p>
+            <p>CodeBooker Team</p>
+        `;
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.MAIL_USERNAME,
+                pass: process.env.MAIL_PASSWORD,
+            },
+        });
+
+        const mailOptions = {
+            from: process.env.MAIL_USERNAME,
+            to: email,
+            subject: 'Password Reset Link',
+            html: message,
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log(error);
+            } else {
+                res.status(200).json('Email sent!');
+                console.log('Email sent: ' + info.response);
+            }
+        });
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json('Something went wrong...');
+    }
+};
+
+module.exports.resetPassword = async (req, res) => {
+    const { password } = { ...req.body };
+    const { token } = { ...req.params };
+
+    try {
+        
+        //Find user with this token in DB
+        const userWithResetToken = await User.findOne({
+            resetPasswordToken: token,
+        });
+    
+        if (!userWithResetToken) {
+            return res.status(401).json({
+                message: 'Token expired or invalid',
+            });
+        }
+
+        // Hash new password
+        const hashPassword = await bcrypt.hash(password, 12);
+        userWithResetToken.password = hashPassword;
+        await userWithResetToken.save();
+
+        res.status(200).json({
+            message: 'Password Reset Successful',
+        });
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).json('Something went wrong...');
+    }
 };
