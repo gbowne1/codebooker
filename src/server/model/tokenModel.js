bd0cd06002d4a08a51714e8c2f6a6c53ddbdc4c0
BlackBond06 <eleazuosita007@yahoo.com>
2023-08-22 20:43:35 +0100

    Create Token model with expiration handling

---
+++ src/server/model/tokenModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const tokenSchema = new mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"User"
+    },
+    token:{
+        type:String,
+        required:true,
+    },
+
+    created_on:{
+        type:Date,
+        required: true
+    },
+    expires_in:{
+        type:Date,
+        required: true,
+        index: { expires: '1s' }
+    }
+});
+
+const Token = mongoose.model('Token', tokenSchema);
+
+module.exports = Token;
