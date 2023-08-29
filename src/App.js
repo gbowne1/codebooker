24c54c42d397978b76e788a92ee5739ba8ee1b55
Denys Sichka <sickadenis@gmail.com>
2023-08-29 16:18:10 +0300

    feature: added privacy-policy page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Register from './components/Register/Register';
 import UserAuthenticated from './ProtectedRoute';
 import ForgotPassword from './pages/ForgotPassword/ForgotPassword';
 import ResetPassword from './pages/ResetPassword/ResetPassword';
+import PrivacyPolicy from './pages/PrivacyPolicy/PrivacyPolicy';
 
 export default function App() {
     return (
@@ -16,6 +17,7 @@ export default function App() {
                 <Route element={<UserAuthenticated />}>
                     <Route path='/' element={<Home />} />
                     <Route path='/profile' element={<Profile />} />
+                    <Route path='/privacy-policy' element={<PrivacyPolicy />} />
                 </Route>
                 <Route path='/login' element={<Login />} />
                 <Route path='/register' element={<Register />} />
