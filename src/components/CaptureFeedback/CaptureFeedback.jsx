1640af25830a5d95e1574ea6ff3eaf90fce403be
Viktor Ristic <vktr.r2@protonmail.com>
2023-08-30 18:28:48 -0400

    Setup CaptureFeedback component and styling files

---
+++ src/components/CaptureFeedback/CaptureFeedback.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./CaptureFeedback.css"
+
+export const CaptureFeedback = () => {
+
+  return (
+    <div className="feedback-panel">
+      <p>TEST TEXT HERE</p>
+    </div>
+  )
+}
