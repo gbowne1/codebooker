6fe0a33252aa7d26f08d5d4f6cc2566ab3624d20
Viktor Ristic <vktr.r2@protonmail.com>
2023-08-30 18:42:08 -0400

    Add state management to component

--- src/components/CaptureFeedback/CaptureFeedback.jsx
+++ src/components/CaptureFeedback/CaptureFeedback.jsx
@@ -3,8 +3,17 @@ import "./CaptureFeedback.css"
 
 export const CaptureFeedback = () => {
 
+  const [panelActive, setPanelActive] = useState(false);
+
+  const togglePanel = () => {
+    setPanelActive((prev) => !prev);
+  };
+
   return (
-    <div className="feedback-panel">
+    <div className={`feedback-panel ${panelActive ? 'active' : ''}`}>
+      <button className="close-button" onClick={togglePanel}>
+        X
+      </button>
       <p>TEST TEXT HERE</p>
     </div>
   )
