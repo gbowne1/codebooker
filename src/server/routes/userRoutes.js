bde4e278423757ee977854a46f27c1653972d1b5
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-06 12:41:04 -0400

    Add login session validation to userRoutes

--- src/server/routes/userRoutes.js
+++ src/server/routes/userRoutes.js
@@ -9,6 +9,14 @@ const {
 } = require('../controller/userController');
 const { allfeedback, addfeedback } = require('../controller/feedBackController');
 
+// Ensure user is logged in.
+function requireLogin(req, res, next) {
+  if (!req.session.userId) {
+      return res.status(401).send('You must be logged in.');
+  }
+  next();
+}
+
 //auth
 router.post('/login', login);
 router.post('/logout', logout);
@@ -20,6 +28,6 @@ router.put('/reset-password/:token', resetPassword);
 
 //feedback routes
 router.get('/feedback/all', allfeedback);
-router.post('/feedback/new', addfeedback);
+router.post('/feedback/new', requireLogin, addfeedback);
 
 module.exports = router;
