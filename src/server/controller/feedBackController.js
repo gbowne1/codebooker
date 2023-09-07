0fee8d5d2ef02c4c5e32650aa73b7bde9c61621d
Viktor Ristic <vktr.r2@protonmail.com>
2023-09-06 23:33:50 -0400

    Refactor addfeedback func to receive author from req.body

--- src/server/controller/feedBackController.js
+++ src/server/controller/feedBackController.js
@@ -1,16 +1,15 @@
 const FeedBack = require('../model/feedBackModel');
 
 module.exports.addfeedback = async (req, res) => {
-  const { feedback, rating } = req.body;
-  const author = req.session.userId;
-  try {
-      const newfeedback = new FeedBack({ feedback, rating, author });
-      await newfeedback.save();
-      res.status(200).json(newfeedback);
-  } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: 'Server Error' });
-  }
+    const { feedback, rating, author } = req.body;
+    try {
+        const newfeedback = new FeedBack({ feedback, rating, author });
+        await newfeedback.save();
+        res.status(200).json(newfeedback);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server Error' });
+    }
 };
 
 module.exports.allfeedback = async (req, res) => {
