21cbbde898064734009c43bb03ca1e9008a1cd25
BlackBond06 <eleazuosita007@yahoo.com>
2023-09-09 13:29:13 +0100

    Add support schema model

---
+++ src/server/model/supportModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const supportSchema = new mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+    },
+    description: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['Open', 'In Progress', 'Resolved'],
+        default: 'Open',
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+const Support = mongoose.model('Support', supportSchema);
+module.exports = Support;
