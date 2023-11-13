63c0258da39c0e4d1656f3b2cc752e9be6d0d315
BlackBond06 <eleazuosita007@yahoo.com>
2023-11-13 12:13:44 +0100

    profile routes

--- src/server/routes/profileRoutes.js
+++ src/server/routes/profileRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const { newProfile } = require('../controller/profileController');
+const { newProfile, getProfile } = require('../controller/profileController');
 router.post('/new-profile', newProfile);
+router.get('/get-profile', getProfile);
 
 module.exports = router;
