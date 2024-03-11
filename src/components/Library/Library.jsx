b1ab4bf3895edb32ea8d690fbe4b7ec6e63c8ba8
George Sparrow <dev.georgios.nikolaidis@gmail.com>
2024-03-11 15:09:54 +0100

    Add to Library button #63

--- src/components/Library/Library.jsx
+++ src/components/Library/Library.jsx
@@ -90,6 +90,11 @@ export default function Library({ filter, setFilter }) {
     const userId = JSON.parse(localStorage.getItem('user'))._id;
     //This state helps us for two way in input filed
     const [isAdded, handleIsAdded] = useState(false);
+    //Add to library modal
+    const [isLibraryModalOpen, setIsLibraryModalOpen] = useState(false);
+    // prettier-ignore
+    const [addToGlobalLibraryMessage, setAddToGlobalLibraryMessage] = useState(null);
+
     const removeBookByName = async (row) => {
         try {
             setLoading(true);
@@ -166,6 +171,21 @@ export default function Library({ filter, setFilter }) {
         }
         setLoading(false);
     };
+
+    const addBookToGlobalLibrary = async (book) => {
+        try {
+            const response = await axios.post(
+                'http://localhost:3001/api/books/add-book-to-global-library',
+                { book }
+            );
+
+            setAddToGlobalLibraryMessage(response.data.payload);
+            setIsLibraryModalOpen(true);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     const addBooksToDB = async (fileBooks) => {
         try {
             const userData = JSON.parse(localStorage.getItem('user'));
@@ -684,6 +704,43 @@ export default function Library({ filter, setFilter }) {
                     </form>
                 </Box>
             </Modal>
+            {/* Add to Library modal */}
+            <Modal
+                open={isLibraryModalOpen}
+                onClose={() => {
+                    setIsLibraryModalOpen(false);
+                }}
+            >
+                {/* prettier-ignore */}
+                <Box
+                  sx={style}
+                  style={{ width: '40%' }}>
+                    <Typography
+                        id='modal-modal-title'
+                        variant='h6'
+                        component='h2'
+                        className={addToGlobalLibraryMessage ?`${Classes.addToGlobalLibrarySuccess}`:`${Classes.addToGlobalLibraryError}`}
+                    >
+                      { addToGlobalLibraryMessage? 'Book successfully added to global library':'Book already exist in  global library'  }
+                      <Box
+                       display={'flex'}
+                       justifyContent={'center'}
+
+                      >
+                      <Button
+                            color='info'
+                            style={{ marginTop: '10px' }}
+                            onClick={() => {
+                                setIsLibraryModalOpen(false);
+                            }}
+                            autoFocus
+                            >
+                            Close
+                        </Button>
+                         </Box>
+                    </Typography>
+                </Box>
+            </Modal>
             {myRows.length > 0 ? (
                 <TableContainer
                     style={{ marginTop: 80, marginInline: 10 }}
@@ -1078,8 +1135,21 @@ export default function Library({ filter, setFilter }) {
                                                 setEnableReviewModal(true);
                                             }}
                                         >
-                                            {' '}
                                             Read Reviews
+                                        </Button>
+                                        <Button
+                                            type='submit'
+                                            variant='contained'
+                                            color='secondary'
+                                            style={{
+                                                marginLeft: '10px',
+                                                width: '80px',
+                                            }}
+                                            onClick={() => {
+                                                addBookToGlobalLibrary(row);
+                                            }}
+                                        >
+                                            Add to library
                                         </Button>
                                     </TableCell>
                                 </TableRow>
--- src/server/routes/booksRoutes.js
+++ src/server/routes/booksRoutes.js
@@ -6,11 +6,13 @@ const {
     newBook,
     deleteBook,
     getAllFromFile,
+    addBookToGlobalLibrary,
 } = require('../controller/booksController');
 const { get } = require('lodash');
 
 router.get('/getall', getAllBooks);
 router.post('/newbook', newBook);
+router.post('/add-book-to-global-library', addBookToGlobalLibrary);
 router.delete('/:id', deleteBook);
 router.get('/filedata', getAllFromFile);
 
