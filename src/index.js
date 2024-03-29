9e7b22df00e016a06ad2f809f0f523b56364671c
BlackBond06 <eleazuosita007@yahoo.com>
2024-03-29 18:24:25 +0100

    Added i18n

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import './pages/Settings/utils/i18n';
 import reportWebVitals from './reportWebVitals';
 import { StyledEngineProvider } from '@mui/styled-engine';
 
