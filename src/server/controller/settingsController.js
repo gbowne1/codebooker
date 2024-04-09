54802ceed88996a6ccf0c11551e6bab6fa611782
BlackBond06 <eleazuosita007@yahoo.com>
2024-04-09 19:33:45 +0100

    initial commit for settings controller

--- src/server/controller/settingsController.js
+++ src/server/controller/settingsController.js
@@ -1,28 +1,44 @@
-const Setting = require('../model/settingsModel');
+const Settings = require('../model/settingsModel');
 const User = require('../model/userModel');
 module.exports.userSettings = async (req, res) => {
-    const {
-        userEmail
-    } = req.body;
+    const {checkedCheckboxes, userEmail} = req.body;
+    const [notification, privacy, reading_preferences, author] = checkedCheckboxes;
+    const extractSetting = (arr, name) =>{
+        return arr?.filter((item)=> item.name === name);
+    }
     try {
         // find user
         const user = await User.findOne({ email:userEmail });
-        if (!user) {
-            return res.status(404).json({
-                message:
-                    'Sorry no user with that email exist(USER NOT FOUND)',
-            });
+        let settings = await Settings.findOne({ userId: user._id });
+        if(!settings){
+            settings = new Settings({
+                userId: user._id,
+                push_notification:{
+                    new_book_release: extractSetting(notification?.values, 'newBook')?.[0]?.status ?? false,
+                    book_recommendation: extractSetting(notification?.values, 'recommendation')?.[0]?.status ?? false,
+                    book_review_update: extractSetting(notification?.values, 'reviews')?.[0]?.status ?? false,
+                },
+                email_notification:{
+                    codebooker_updates: extractSetting(notification?.values, 'updates')?.[0]?.status ?? false,
+                    tips: extractSetting(notification?.values, 'tips')?.[0]?.status ?? false,
+                    suggestions: extractSetting(notification?.values, 'suggestions')?.[0]?.status ?? false,
+                },
+                privacy: {
+                    keep_profile_private: extractSetting(privacy?.values, 'profile-visibility')?.[0]?.status ?? false,
+                    friends_only: extractSetting(privacy?.values, 'friends_only')?.[0]?.status ?? false,
+                    public: extractSetting(privacy?.values, 'public')?.[0]?.status ?? false,
+                },
+                reading_preferences: {
+                    favorite_genre: reading_preferences?.values ?? [],
+                    favorite_author: author?.values ?? [],
+                }
+            })
         }
-        // Save settings to db
-        const userSettings = new Setting({
-            created_at: Date.now(),
-        });
-        await userSettings.save();
-        res.status(200).json({
-            message: 'User Settings Saved Successful',
-        });
+        await settings.save();
+        res.status(200).json({ message: 'Settings updated successfully' });
     } catch (err) {
-        console.error(err);
+        console.error('Error updating settings:', err);
         res.status(500).json({ message: 'Internal server error' });
     }
+
 };
