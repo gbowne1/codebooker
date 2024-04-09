64ae112e56ad38e96804222c839b67961454febc
BlackBond06 <eleazuosita007@yahoo.com>
2024-04-09 19:26:38 +0100

    renamed stom variable

--- src/components/Hooks/useCheckBoxToggle.js
+++ src/components/Hooks/useCheckBoxToggle.js
@@ -1,25 +1,58 @@
 import { useState } from 'react';
-import { change } from '../../pages/Settings/utils/alert';
 import { useRecoilState } from 'recoil';
+import { userSettingsAtom } from '../../pages/Settings/utils/alert';
 
 const useCheckboxToggle = () => {
-    const [names, setNames] = useState([]);
     const [error, setError] = useState(false);
     const [inputValue, setInputValue] = useState('');
-    const [checkedCheckboxes, setCheckedCheckboxes] = useRecoilState(change);
-
-    const toggleSwitch = (e) => {
+    const [checkedCheckboxes, setCheckedCheckboxes] =
+        useRecoilState(userSettingsAtom);
+    const toggleSwitch = (e, type) => {
         const isChecked = e.target.checked;
         const id = e.target.name;
         if (isChecked) {
-            setCheckedCheckboxes((prevObj) => [
-                ...prevObj,
-                { name: id, status: isChecked },
-            ]);
+            setCheckedCheckboxes((prevObj) => {
+                const updatedArrValues = [...prevObj];
+                const existingIndex = updatedArrValues.findIndex(
+                    (item) => item.name === type
+                );
+                if (existingIndex !== -1) {
+                    updatedArrValues[existingIndex] = {
+                        ...updatedArrValues[existingIndex],
+                        values: [
+                            ...updatedArrValues[existingIndex].values,
+                            { name: id, status: isChecked },
+                        ],
+                    };
+                } else {
+                    updatedArrValues.push({
+                        name: type,
+                        values: [{ name: id, status: isChecked }],
+                    });
+                }
+                return updatedArrValues;
+            });
         } else {
-            setCheckedCheckboxes((prevObj) =>
-                prevObj.filter((checkedId) => checkedId.name !== id)
-            );
+            setCheckedCheckboxes((prevObj) => {
+                const updatedArrValues = [...prevObj];
+                const existingIndex = updatedArrValues.findIndex(
+                    (item) => item.name === type
+                );
+                if (existingIndex !== -1) {
+                    const newArray = updatedArrValues[
+                        existingIndex
+                    ].values.filter((value) => value.name !== id);
+                    if (newArray.length === 0) {
+                        updatedArrValues.splice(existingIndex, 1);
+                    } else {
+                        updatedArrValues[existingIndex] = {
+                            ...updatedArrValues[existingIndex],
+                            values: newArray,
+                        };
+                    }
+                }
+                return updatedArrValues;
+            });
         }
     };
 
@@ -27,14 +60,45 @@ const useCheckboxToggle = () => {
         const isChecked = e.target.checked;
         const id = e.target.id;
         if (isChecked) {
-            setCheckedCheckboxes((prevObj) => [
-                ...prevObj,
-                { name: id, status: isChecked },
-            ]);
+            setCheckedCheckboxes((prevObj) => {
+                const updatedArrValues = [...prevObj];
+                const existingIndex = updatedArrValues.findIndex(
+                    (item) => item.name === 'reading-preferences'
+                );
+                if (existingIndex !== -1) {
+                    updatedArrValues[existingIndex] = {
+                        ...updatedArrValues[existingIndex],
+                        values: [...updatedArrValues[existingIndex].values, id],
+                    };
+                } else {
+                    updatedArrValues.push({
+                        name: 'reading-preferences',
+                        values: [id],
+                    });
+                }
+                return updatedArrValues;
+            });
         } else {
-            setCheckedCheckboxes((prevObj) =>
-                prevObj.filter((checkedId) => checkedId.name !== id)
-            );
+            setCheckedCheckboxes((prevObj) => {
+                const updatedArrValues = [...prevObj];
+                const existingIndex = updatedArrValues.findIndex(
+                    (item) => item.name === 'reading-preferences'
+                );
+                if (existingIndex !== -1) {
+                    const newArray = updatedArrValues[
+                        existingIndex
+                    ].values.filter((value) => value !== id);
+                    if (newArray.length === 0) {
+                        updatedArrValues.splice(existingIndex, 1);
+                    } else {
+                        updatedArrValues[existingIndex] = {
+                            ...updatedArrValues[existingIndex],
+                            values: newArray,
+                        };
+                    }
+                }
+                return updatedArrValues;
+            });
         }
     };
 
@@ -42,18 +106,19 @@ const useCheckboxToggle = () => {
         setInputValue(e.target.value);
     };
     const addAuthor = () => {
-        if (names.includes(inputValue)) {
+        const existingIndex = checkedCheckboxes.findIndex(
+            (item) => item.name === 'authors'
+        );
+        const authorsArr = checkedCheckboxes[existingIndex]?.values;
+        if (authorsArr?.includes(inputValue)) {
             setError(true);
             setInputValue('');
             return;
         }
         if (inputValue.trim() !== '') {
-            setNames([...names, inputValue.trim()]);
+            // setNames([...names, inputValue.trim()]);
             setCheckedCheckboxes((prevObj) => {
                 const updatedArrValues = [...prevObj];
-                const existingIndex = updatedArrValues.findIndex(
-                    (item) => item.name === 'authors'
-                );
                 if (existingIndex !== -1) {
                     updatedArrValues[existingIndex] = {
                         ...updatedArrValues[existingIndex],
@@ -70,36 +135,36 @@ const useCheckboxToggle = () => {
                 }
                 return updatedArrValues;
             });
-            // setCheckedCheckboxes([
-            //     ...checkedCheckboxes,
-            //     ...names,
-            //     inputValue.trim(),
-            // ]);
             setInputValue('');
         }
     };
     const removeAuthorName = (nameToRemove) => {
-        setNames(names.filter((name) => name !== nameToRemove));
         setCheckedCheckboxes((prevObj) => {
             const updatedArrValues = [...prevObj];
             const existingIndex = updatedArrValues.findIndex(
                 (item) => item.name === 'authors'
             );
-            return updatedArrValues[existingIndex].values.filter(
-                (name) => name !== nameToRemove
-            );
+            if (existingIndex !== -1) {
+                const newArray = updatedArrValues[existingIndex].values.filter(
+                    (value) => value !== nameToRemove
+                );
+                if (newArray.length === 0) {
+                    updatedArrValues.splice(existingIndex, 1);
+                } else {
+                    updatedArrValues[existingIndex] = {
+                        ...updatedArrValues[existingIndex],
+                        values: newArray,
+                    };
+                }
+            }
+            return updatedArrValues;
         });
-        // setCheckedCheckboxes(
-        //     [...checkedCheckboxes].filter((name) => name !== nameToRemove)
-        // );
     };
 
     return {
-        names,
         error,
         setError,
         inputValue,
-        setNames,
         checkedCheckboxes,
         setCheckedCheckboxes,
         toggleSwitch,
