4ba14548206018bff3a9de10015440ffc0f95e80
BlackBond06 <eleazuosita007@yahoo.com>
2024-04-10 16:00:00 +0100

    updated controller function

--- src/server/controller/settingsController.js
+++ src/server/controller/settingsController.js
@@ -7,7 +7,7 @@ module.exports.userSettings = async (req, res) => {
         return arr?.filter((item)=> item.name === name);
     }
     try {
-        // find user
+        // find settings
         const user = await User.findOne({ email:userEmail });
         let settings = await Settings.findOne({ userId: user._id });
         if(!settings){
@@ -33,6 +33,19 @@ module.exports.userSettings = async (req, res) => {
                     favorite_author: author?.values ?? [],
                 }
             })
+        }  else {
+            // Update existing settings
+            settings.push_notification.new_book_release = extractSetting(notification?.values, 'newBook')?.[0]?.status ?? false;
+            settings.push_notification.book_recommendation = extractSetting(notification?.values, 'recommendation')?.[0]?.status ?? false
+            settings.push_notification.book_review_update = extractSetting(notification?.values, 'reviews')?.[0]?.status ?? false;
+            settings.email_notification.codebooker_updates = extractSetting(notification?.values, 'updates')?.[0]?.status ?? false;
+            settings.email_notification.tips = extractSetting(notification?.values, 'tips')?.[0]?.status ?? false;
+            settings.email_notification.suggestions = extractSetting(notification?.values, 'suggestions')?.[0]?.status ?? false;
+            settings.privacy.keep_profile_private = extractSetting(privacy?.values, 'profile-visibility')?.[0]?.status ?? false;
+            settings.privacy.friends_only = extractSetting(privacy?.values, 'friends_only')?.[0]?.status ?? false;
+            settings.privacy.public = extractSetting(privacy?.values, 'public')?.[0]?.status ?? false;
+            settings.reading_preferences.favorite_genre = reading_preferences?.values ?? settings.reading_preferences.favorite_genre;
+            settings.reading_preferences.favorite_author = author?.values ?? settings.reading_preferences.favorite_author;
         }
         await settings.save();
         res.status(200).json({ message: 'Settings updated successfully' });
